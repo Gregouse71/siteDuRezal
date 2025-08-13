b9cf0079bdcb7f6cca12ac29a29d74e10180311f
Grégoire Girardet <gregoire.girardet@laposte.net>
2025-08-13 22:02:02 +0200

    Ajout CORS

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from user_router import user_router
 from authentication import auth_router
 from wifi import wifi_router
-from user_listing import listing_router
+from listing_router import listing_router
 
-app = FastAPI(root_path="/api")
+app = FastAPI(
+    root_path="/api"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router (user_router)
 app.include_router (auth_router)
