744a71d8446b82498c6eb48a240e5f4bbd1fff6e
Grégoire Girardet <gregoire.girardet@laposte.net>
2025-09-01 15:39:34 +0200

    Pas besoin de filtrer les users

--- backend/listing_router.py
+++ backend/listing_router.py
@@ -28,7 +28,7 @@ class GetUser (SQLModel):
     email: EmailStr | None = None
 
 
-@listing_router.get ("/all")
+@listing_router.get ("/alluids")
 async def get_all (
     current_user: Annotated[User, Depends(get_current_user)]
 ) -> list [str]:
@@ -48,21 +48,26 @@ async def get_all (
         return users
 
 
-@listing_router.get ("/filter")
+@listing_router.get ("/all")
 async def filter_user (
-    filters: GetUser,
     current_user: Annotated[User, Depends(get_current_user)]
 ):
     """
     Renvoie tous les utilisateurs qui matchent avec les paramètres envoyés
     """
+    if not current_user.is_admin:
+        raise HTTPException (
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Vous n'avez pas les droits pour réaliser cette action"
+        )
     with Session (engine) as session:
-        statement = select (User).where (filters.uid is None or col (User.uid).contains (filters.uid),
-                                         filters.is_admin is None or col (User.is_admin) == filters.is_admin,
-                                         filters.acces_wifi is None or col (User.acces_wifi) == filters.acces_wifi,
-                                         filters.is_admin is None or col (User.is_admin) == filters.is_admin,
-                                         filters.nom is None or col (User.nom) == filters.nom,
-                                         filters.prenom is None or col (User.prenom) == filters.prenom,
-                                         filters.email is None or col (User.email) == filters.email,)
+        # statement = select (User).where (filters.uid is None or col (User.uid).contains (filters.uid),
+        #                                  filters.is_admin is None or col (User.is_admin) == filters.is_admin,
+        #                                  filters.acces_wifi is None or col (User.acces_wifi) == filters.acces_wifi,
+        #                                  filters.is_admin is None or col (User.is_admin) == filters.is_admin,
+        #                                  filters.nom is None or col (User.nom) == filters.nom,
+        #                                  filters.prenom is None or col (User.prenom) == filters.prenom,
+        #                                  filters.email is None or col (User.email) == filters.email,)
+        statement = select (User)
         users = session.exec (statement).all ()
         return users
