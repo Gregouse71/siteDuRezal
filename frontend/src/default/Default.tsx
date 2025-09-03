52b7b539a37934e294706cbbc8ffcd20f2b4ffa9
Grégoire Girardet <gregoire.girardet@laposte.net>
2025-09-03 17:40:20 +0200

    Texte page d'accueil

--- frontend/src/default/Default.tsx
+++ frontend/src/default/Default.tsx
@@ -18,18 +18,18 @@ export function Default() {
         <div className="bande">
             {windowWidth > 767 && <img src={arrivee} alt="arrivant" className="image"/>}
             <div className="container-description">
-                <h1> Tu viens d'arriver ? </h1>
-                <p> Pour avoir accès à internet tu dois te créer un compte qui sera ensuite accepté par le Rezal
-                    La cotisation est de 5€ par trimestre, tu peux payer en chambre ou dans la boite aux lettres 420 </p>
+                <h1>Tu viens d'arriver ?</h1>
+                <p> Pour avoir accès à internet tu dois te créer un compte, et cotiser à l'association Rézal.
+                    La cotisation est de 5€ par trimestre, tu peux payer par HelloAsso ou directement auprès d'un membre du bureau.</p>
                 <Link to="resident/register"><Button className="btn-navigation" variant="outlined" color="success"> Je crée mon compte  </Button></Link>
             </div>
         </div>
 
         <div className="bande">
             <div className="container-description">
-                <h1> Tu as déjà un compte ? </h1>
-                <p> Accède à ton compte pour vérifier ou modifier tes informations personnelles </p>
-                <Link to="resident/login"><Button className="btn-navigation" variant="outlined" color="success"> Je me connecte  </Button></Link>
+                <h1>Tu as déjà un compte ?</h1>
+                <p>Accède à ton compte pour vérifier tes informations personnelles.</p>
+                <Link to="resident/login"><Button className="btn-navigation" variant="outlined" color="success">Je me connecte</Button></Link>
             </div>
             {windowWidth > 767 && <img src={baieServeur} alt="baie-serveur" className="image"/>}
         </div>
