b7734bba2b8710ca16a46e3de631a293c80b61a7
Grégoire Girardet <gregoire.girardet@laposte.net>
2025-09-13 13:55:59 +0200

    Lecture de la notifiaction helloasso pour ajouter des credits

--- backend/helloasso.py
+++ backend/helloasso.py
@@ -1,11 +1,30 @@
-from fastapi import Depends, HTTPException, status, APIRouter
+from fastapi import Depends, HTTPException, status, APIRouter, Request
+from sqlmodel import SQLModel, Session, select
+
+from database import User, engine, UserUpdate, patch_user_db
 
 helloasso_router = APIRouter (
     prefix="/helloasso"
 )
 
 @helloasso_router.post ("")
-def post_helloasso (
-    data
+async def post_helloasso (
+    request: Request
 ):
-    print (data)
+    req = await request.json ()
+    data = req["data"]
+    if not (
+        req["eventType"] == "Payment"
+        and data["cashOutState"] == "Transfered"
+    ):
+        pass
+    else:
+        with Session (engine) as session:
+            statement = select (User).where (User.email == data["payer"]["email"])
+            user = session.exec (statement).one ()
+            user.credits = len(data["items"]) * 5
+            session.add (user)
+            session.commit ()
+            print ("done")
+
+    return True
