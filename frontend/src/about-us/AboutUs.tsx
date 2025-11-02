934774bbac57046396ef22c541e9821cc0b392f2
Grégoire Girardet <gregoire.girardet@laposte.net>
2025-11-02 19:48:46 +0100

    Chromebook

--- frontend/src/about-us/AboutUs.tsx
+++ frontend/src/about-us/AboutUs.tsx
@@ -20,6 +20,7 @@ export function AboutUs() {
                 <li>Pour les Mac : téléchargez <a style={{ color: "blue", fontSize: "1em" }} href={`${BACKEND_BASE_URL}/apple_profile`}>ce profil</a>, allez dans <em>Réglages système</em>, cherchez <em>Profils</em>, double-cliquez sur le profil nommé Rézal, et sélectionnez <em>Installer</em>.</li>
                 <li>Pour les iPhones : téléchargez <a style={{ color: "blue", fontSize: "1em" }} href={`${BACKEND_BASE_URL}/apple_profile`}>ce profil</a> allez dans <em>Réglages</em>, <em>Profil téléchargé</em>, rentrez le code de votre téléphone, puis vos identifiants du Rézal.</li>
                 <li>Pour les appareils Android : lors de la connexion, il faut sélectionner TTLS en <em>Méthode EAP</em>, PAP en <em>Application étape 2</em>, et Faire confiance lors de la première connexion comme <em>Certificat CA</em>. Remplissez vos identifiants Rézal dans <em>Identité</em> et <em>Mot de passe</em>. Si vous ne pouvez pas Faire confiance à la première connexion, sélectionnez <em>Certificats du système</em> et choisisez de faire confiance au domaine <em>rezal-mdm.com</em></li>
+                <li>Pour les chromebook : Renseignez les mêmes paramètres que pour Android. Pour le certificat, utiliser <em>Par défaut</em>. Pour <em>Correspondance du suffixe de domaine</em> reiseignez <em>rezal-mdm.com</em></li>
             </ul>
             
         </div>
