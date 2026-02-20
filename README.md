65cda7e101b4dc6900721bcd7a652f4f47905f38
Grégoire Girardet <gregoire.girardet@laposte.net>
2026-02-20 20:02:51 +0100

    Mise à jour CI

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Le frontend a été récupéré de la cersion précédente du site.
 ## Backend
 
 Le backend a été entièrement réécrit. Il est conçu pour réaliser l'authentification des utilisateurs avec le protocole LDAP avec le serveur du Rézal. Ainsi la vérification de l'identité des utilisateurs est centralisée, et peut être réutilisée par d'autres services. Une base de données est tout de même nécessaire utilisée pour stocker les informations des utilisateurs propres uniquement au Rézal.
+
+## Déploiement
+
+Pour utiliser les runner gitlab, il faut placer le script *update_site.sh* dans le répertoir */home/rezal*, et ajouter ceci au début de la ligne de la clé ssh de gitlab-runner : `command="/home/rezal/update_site",no-port-forwarding,no-agent-forwarding,no-X11-forwarding`
--- update_site.sh
+++ update_site.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
-# /home/rezal/update_site.sh
 # Script de mise à jour du site du Rézal, à partir du repo GitLab
 # Par Mathis Liens et Grégoire Girardet
-set -e
-# Restauration du fichier .env
-cp /home/rezal/.env /home/rezal/site_rezal2025/backend
-cd /home/rezal/site_rezal2025
 
-#On arrête le backend, juste au cas où il soit un peu sensible au remplacement de code
-echo "Arrêt des services"
-systemctl stop --user site-rezal-backend
+# Si c'est la copie, on utilise rrsync pour limiter les possibilités
+if [[ "$SSH_ORIGINAL_COMMAND" == rsync* ]]; then
+    /usr/bin/rrsync -wo /home/rezal/site_rezal2025
 
-echo "Mise à jour des dépendances javascript front..."
-cd frontend
-npm install --omit=dev
-
-echo "Building front..."
-npm run build
-cp -r build/* /var/www/site_rezal
-
-echo "Done"
-
-#Au tour du back
-cd ../backend
-echo "Téléchargement des nouvelles dépendance backend"
-/home/rezal/miniconda3/bin/conda env update --file environment.yml --prune --quiet
-echo "Fini"
-
-echo "restarting backend"
-systemctl start --user site-rezal-backend
-
-echo "Fini"
-#Sleep 2 histoire que les services aient le temps de planter si nécessaire, sinon tant qu'ils démarrent le statut est forcément bon
-sleep 3
-systemctl status --user "site-rezal-backend"
+# Si c'est la mise à jour
+elif [[ "$SSH_ORIGINAL_COMMAND" == "update-site" ]]; then
+    set -e
+    # Restauration du fichier .env
+    cp /home/rezal/.env /home/rezal/site_rezal2025/backend
+    cd /home/rezal/site_rezal2025
+    
+    #On arrête le backend, juste au cas où il soit un peu sensible au remplacement de code
+    echo "Arrêt des services"
+    systemctl stop --user site-rezal-backend
+    
+    echo "Mise à jour des dépendances javascript front..."
+    cd frontend
+    npm install --omit=dev
+    
+    echo "Building front..."
+    npm run build
+    cp -r build/* /var/www/site_rezal
+    
+    echo "Done"
+    
+    #Au tour du back
+    cd ../backend
+    echo "Téléchargement des nouvelles dépendance backend"
+    /home/rezal/miniconda3/bin/conda env update --file environment.yml --prune --quiet
+    echo "Fini"
+    
+    echo "restarting backend"
+    systemctl start --user site-rezal-backend
+    
+    echo "Fini"
+    #Sleep 2 histoire que les services aient le temps de planter si nécessaire, sinon tant qu'ils démarrent le statut est forcément bon
+    sleep 10
+    systemctl status --user "site-rezal-backend"
+else
+    echo Commande invalide
+fi
