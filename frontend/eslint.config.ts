728a030421f2ebb3a726655b6923e34fa050bb2f
Mathis Liens <mathis.liens@etu.minesparis.psl.eu>
2026-06-20 00:04:07 -0700

    Ajout des scripts d'initialisations

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -2,12 +2,13 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  globalIgnores(["build/", "node_modules/"]),
   {
     rules: {
       "react/react-in-jsx-scope": "off",
